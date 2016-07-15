f80456ac6c9d4b3c1f204d4e1f335a7c8acdbe04
zebuline <zebuline@crocobox.org>
2016-07-15 22:53:14 +0200

    remove deprecated patterns in urls.py

--- demo/django_anysign_demo/signature_urls.py
+++ demo/django_anysign_demo/signature_urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from django.views.generic import TemplateView
 
 from django_anysign_demo import views
@@ -9,9 +9,8 @@ return_view = TemplateView.as_view(template_name='signer_return.html')
 callback_view = TemplateView.as_view(template_name='callback.html')
 
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'signer/(?P<pk>[0-9]+)/$', signer_view, name='signer'),
     url(r'signer/(?P<pk>[0-9]+)/return/$', return_view, name='signer_return'),
     url(r'callback/(?P<pk>[0-9]+)/$', callback_view, name='callback'),
-)
+]
--- demo/django_anysign_demo/urls.py
+++ demo/django_anysign_demo/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.views.generic import TemplateView
 
 from django_anysign_demo import views
@@ -8,12 +8,11 @@ home_view = TemplateView.as_view(template_name='home.html')
 send_view = views.SendView.as_view()
 
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^signature/',
         include('django_anysign_demo.signature_urls',
                 app_name='anysign',
                 namespace='anysign')),
     url(r'^send/$', send_view, name='send'),
     url(r'^$', home_view, name='home')
-)
+]
